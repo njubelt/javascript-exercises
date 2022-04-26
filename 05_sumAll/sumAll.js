b5311cb0d8855a09b776c3e9cba7d33a0d6b96e0
Nicholas Jubelt <nickjubelt@gmail.com>
2022-04-26 12:54:11 -0400

    Complete java exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    let int3 = 0;
+    if (int1 > int2) {
+        int3 = int2; //set intermediate variable
+        int2 = int1; //swap int2 and int1
+        int1 = int3; //set int1 to intermediate variable
+    }
+    if ((int1 < 0) || (int2 < 0) || (typeof int1 != "number") || (typeof int2 != "number")) {
+        return "ERROR";
+    }
+    for (let i = int1; i <= int2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
