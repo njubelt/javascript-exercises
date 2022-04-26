74fcef3f2965c8832fcede1a1e24cfa4fb6f89f9
Nicholas Jubelt <nickjubelt@gmail.com>
2022-04-26 13:13:57 -0400

    Add ctof and ftoc functions for conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = 0;
+  tempC = Math.round(((tempF-32) * (5/9))*10)/10;
+  return tempC;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = 0;
+  tempF = Math.round(((tempC * (9/5)) + 32)*10)/10;
+  return tempF;
 };
 
 // Do not edit below this line
